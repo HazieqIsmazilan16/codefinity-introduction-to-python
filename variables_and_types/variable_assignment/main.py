d2f391475c86e8441bda8c8b8c23fa60ac76e719
HazieqIsmazilan16 <>
2026-05-26 17:30:44 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,12 @@
+
+
+
+# Testing
+#print("Item : ", item_name)
+item_name = "Eggs"
+#print("Price per unit: $", item_price)
+item_price = 3.99
+#print("quantity in stock: ", item_stock_quantity)
+item_stock_quantity = 120
+#print("In stock: ", in_stock)
+in_stock = True
