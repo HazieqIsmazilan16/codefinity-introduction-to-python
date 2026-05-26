6f8ca64e5bd6422b320081a1b15b3af36e94a996
HazieqIsmazilan16 <>
2026-05-26 17:37:27 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,11 @@
+
+
+
+# Testing
+#print("Item:", ___)
+item_name = "Bread"
+#print("Price per loaf: $", ___)
+price_per_loaf = 4.52
+#print("Quantity in stock:", ___)
+quantity_in_stock = 230
+print(item_name, price_per_loaf, quantity_in_stock)
