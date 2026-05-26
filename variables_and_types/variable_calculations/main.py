6c7960181a194868c3eaf0e128351513550257fa
HazieqIsmazilan16 <>
2026-05-26 17:43:02 +0000

    Variable calculations

---
+++ variables_and_types/variable_calculations/main.py
@@ -0,0 +1,13 @@
+
+
+
+# Testing
+item_name = "Soda"           # 1. assign the item name
+item_price = 6.99            # 2. assign the price per case
+purchase_quantity = 5        # 3. assign the number of cases
+total_cost = item_price * purchase_quantity   # 4. calculate total cost
+
+# 5. now you can print
+print("Total cost for", purchase_quantity, "cases of", item_name, "is $", total_cost)
+
+print("Total cost for", purchase_quantity, "cases of", item_name, "is $", total_cost)
